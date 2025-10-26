f9f2d2ba008a5ec68ab3ef2233199248f1cba84d
aaditmunjal <aaditmunjal@gmail.com>
2025-10-26 15:07:24 -0500

    Added baseline vgg code

---
+++ vgg.py
@@ -0,0 +1,105 @@
+# This is the base VGG model developed by Karen Simonyan & Andrew Zisserman with a few changes:
+# 1) 7x7 adaptive average pooling after the convolution layers - makes the model input-size agnostic
+# 2) Batch Normalization
+# 3) Kaiming initialization
+
+# This model will act as the baseline for experiments conducted in this project
+# It is directly based on the code provided by PyTorch (TorchVision)
+
+import torch
+import torch.nn as nn
+from typing import Union, List, Dict, Any, cast
+
+
+class VGG(nn.Module):
+    def __init__(
+        self, features: nn.Module, num_classes: int = 1000, init_weights: bool = True, dropout: float = 0.5
+    ) -> None:
+        super().__init__()
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+def make_layers(cfg: List[Union[str, int]], batch_norm: bool = False) -> nn.Sequential:
+    layers: List[nn.Module] = []
+    in_channels = 3
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            v = cast(int, v)
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfgs: Dict[str, List[Union[str, int]]] = {
+    "A": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "B": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "D": [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M"],
+    "E": [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"],
+}
+
+
+def _vgg(cfg: str, batch_norm: bool, **kwargs: Any) -> VGG:
+    model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm), **kwargs)
+    return model
+
+
+def vgg11(**kwargs: Any) -> VGG:
+    return _vgg("A", False, **kwargs)
+
+def vgg11_bn(**kwargs: Any) -> VGG:
+    return _vgg("A", True, **kwargs)
+
+def vgg13(**kwargs: Any) -> VGG:
+    return _vgg("B", False, **kwargs)
+
+def vgg13_bn(**kwargs: Any) -> VGG:
+    return _vgg("B", True, **kwargs)
+
+def vgg16(**kwargs: Any) -> VGG:
+    return _vgg("D", False, **kwargs)
+
+def vgg16_bn(**kwargs: Any) -> VGG:
+    return _vgg("D", True, **kwargs)
+
+def vgg19(**kwargs: Any) -> VGG:
+    return _vgg("E", False, **kwargs)
+
+def vgg19_bn(**kwargs: Any) -> VGG:
+    return _vgg("E", True, **kwargs)
